393a882c5090074e514edb701585a5e51c6796a8
winstonkiprop <winstonekip50@gmail.com>
2022-05-15 20:58:26 +0300

    working on .js file

---
+++ Assets/index.js
@@ -0,0 +1,19 @@
+function result(){
+
+    let score=0;
+    if (document.getElementById(right1).checked)
+    {
+          score++;    }
+          if (document.getElementById(right1).checked)
+          {
+                score++;    }
+                if (document.getElementById(right1).checked)
+                {
+                      score++;    }
+                      if (document.getElementById(right1).checked)
+                      {
+                            score++;    }
+                            if (document.getElementById(right1).checked)
+                            {
+                                  score++;    }
+}
