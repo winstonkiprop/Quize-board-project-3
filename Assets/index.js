5ec25cfcc818531e896577f6bdf17ea01edd2527
winstonkiprop <winstonekip50@gmail.com>
2022-05-15 23:26:54 +0300

    writing js file

--- Assets/index.js
+++ Assets/index.js
@@ -1,27 +1,36 @@
-function result(){
-
-    var score=0;
-         if (document.getElementById(right1).checked)
-                      {
-          score++;    }
-          if (document.getElementById(right2).checked)
-             {
-                score++;   
-             }
-          if (document.getElementById(right3).checked)
-                                 {
-                      score++;    }
-          if (document.getElementById(right4).checked)
-                                        {
-                            score++;    }
-         if (document.getElementById(right5).checked)
-                                             {
-                                  score++;    }
-                                 alert("your score is;"+score)
-                                 
-
-
-
-
-
+function result() {
+    let score=0;
+    let percentage=0;
+    let calculate_percent=0;
+   if(document.getElementById('right1').checked)
+   {
+       score++; 
+       }
+       if(document.getElementById('right2').checked)
+   {
+       score++; 
+       }  if(document.getElementById('right3').checked)
+   {
+       score++; 
+       }
+       if(document.getElementById('right4').checked)
+   {
+       score++; 
+       }  if(document.getElementById('right5').checked)
+   {
+       score++; 
+       }
+       
+       alert ("your score is:"+score)
+       calculate_percent=score/5;
+       percentage= score/6*100;
+       if(percentage>80){
+           document.write("" + "[performed excellency]" );
+       }
+       else if(percentage<=50 && percentage<=80){
+           document.write("" + "[passed fairly]")
+       }
+       else{
+           document.write("" + "[performed poorly]");
+       }
 }
