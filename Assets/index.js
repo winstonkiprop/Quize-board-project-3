2b4b7679f68dbcdafef258d6ca9ddb0f3214dcfb
winstonkiprop <winstonekip50@gmail.com>
2022-05-15 21:16:31 +0300

    working on .js file

--- Assets/index.js
+++ Assets/index.js
@@ -1,19 +1,25 @@
 function result(){
 
-    let score=0;
-    if (document.getElementById(right1).checked)
-    {
-          score++;    }
-          if (document.getElementById(right1).checked)
-          {
-                score++;    }
-                if (document.getElementById(right1).checked)
-                {
-                      score++;    }
-                      if (document.getElementById(right1).checked)
+    var score=0;
+         if (document.getElementById(right1).checked)
                       {
+          score++;    }
+          if (document.getElementById(right2).checked)
+             {
+                score++;   
+             }
+          if (document.getElementById(right3).checked)
+                                 {
+                      score++;    }
+          if (document.getElementById(right4).checked)
+                                        {
                             score++;    }
-                            if (document.getElementById(right1).checked)
-                            {
+         if (document.getElementById(right5).checked)
+                                             {
                                   score++;    }
+                                  alert("your score is:"+score);
+
+
+
+
 }
