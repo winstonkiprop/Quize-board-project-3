51ab6d87ea72a9fb64ddc7e0df6859b21df738e2
winstonkiprop <winstonekip50@gmail.com>
2022-05-16 12:17:27 +0300

    branching

--- Assets/index.js
+++ Assets/index.js
@@ -4,30 +4,30 @@ function result() {
     let calculate_percent=0;
    if(document.getElementById('right1').checked)
    {
-       score++; 
+       score+=20; 
        }
        if(document.getElementById('right2').checked)
    {
-       score++; 
+       score+=20; 
        }  if(document.getElementById('right3').checked)
    {
-       score++; 
+       score+=20; 
        }
        if(document.getElementById('right4').checked)
    {
-       score++; 
+       score+=20; 
        }  if(document.getElementById('right5').checked)
    {
-       score++; 
+       score+=20; 
        }
        
-       alert ("your score is:"+score)
+       alert ("your score is:"+score+"%")
        calculate_percent=score/5;
        percentage= score/5*100;
        if(percentage>80){
-           document.write("" + "[performed excellency]" );
+           document.write(" " + "[performed excellently]" );
        }
-       else if(percentage<=50 && percentage<=80){
+       else if(percentage>=50 && percentage<=80){
            document.write("" + "[passed fairly]")
        }
        else{
